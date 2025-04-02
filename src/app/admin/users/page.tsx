57d7cf1f9857f42dad05588275640a3b31293941
フィルム <waranthorn_c@outlook.com>
2025-04-02 23:41:02 +0700

    Update page.tsx

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,13 @@
+// src/app/admin/users/page.tsx
+"use client";
+
+import React from "react";
+
+export default function UsersPage() {
+  return (
+    <div>
+      <h1>ผู้ใช้งานทั้งหมด</h1>
+      {/* เนื้อหาหน้า Users */}
+    </div>
+  );
+}
